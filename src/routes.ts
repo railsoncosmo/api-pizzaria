3e53fadd34407b8de78760c637ff4ebbc426d0c3
railsoncosmo <railson_sousa10@icloud.com>
2025-02-18 16:40:57 -0300

    Adicionando as rotas de product

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
-import { CreateCategoryController } from './controllers/category/CreatecategoryController';
+
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+
+import { CreateProductController } from './controllers/product/CreateProductController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
+import uploadConfig from './config/multer';
+
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
 
 
 // routes user
@@ -19,5 +26,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 // router category
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+// router product
+router.post('/product', isAuthenticated, upload.single, new CreateProductController().handle)
 
 export { router }; 
