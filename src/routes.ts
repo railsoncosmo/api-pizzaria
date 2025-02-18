b41258aebc866fd007f2f4359deffbe17a0218f7
railsoncosmo <railson_sousa10@icloud.com>
2025-02-18 11:40:42 -0300

    implementando as funcionalidades de cadastrar e listar as categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,21 @@ import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
+import { CreateCategoryController } from './controllers/category/CreatecategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 
-//routes user
+// routes user
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
+// router category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router }; 
--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
         })
     }
 
-    return res.status(500 ).json({
+    return res.status(500).json({
         status: 'error',
         message: 'Internal server error.'
     })
