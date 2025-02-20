5e39520e417ef30ec9676b90eccaf123c3262c70
railsoncosmo <railson_sousa10@icloud.com>
2025-02-20 00:10:46 -0300

    Finalizando a api pizzaria

--- src/routes.ts
+++ src/routes.ts
@@ -13,12 +13,14 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
-
-import { AddItemController } from './controllers/order/AddItemController';
-import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrderController } from './controllers/order/ListOrderController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
+
+import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -49,5 +51,6 @@ router.delete('/order/remove', isAuthenticated, new RemoveItemController().handl
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrderController().handle)
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.put ('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export { router };
